656cbf63129e87c6decad1c3128db2c552e24185
Aleff-81 <alegifi2004@gmail.com>
2026-01-10 12:26:59 -0300

    Atualização completa

---
+++ f_app_modules/jogadores.py
@@ -0,0 +1,199 @@
+from datetime import datetime
+import streamlit as st
+import pandas as pd
+from f_app_modules.salvar_dados import salvar_dados
+from f_app_modules.variaveis_arquivo import variaveis
+
+arquivo_jogadores, arquivo_financeiro, c, d = variaveis()
+
+def adicionar_jogador_ui():
+    """Interface para adicionar um novo jogador (mensalista ou convidado)."""
+    st.subheader("🤝 Cadastrar jogador", anchor="cadastrar-jogador")
+    st.markdown("[Ver lista de jogadores](#lista_jogadores)")
+    
+    JOGADORES = st.session_state['jogadores']
+    FINANCEIRO = st.session_state['financeiro']
+    
+    VALOR_MENSALIDADE_ATUAL = FINANCEIRO['config']['mensalidade_atual']
+
+    with st.form("cadastro_jogador", clear_on_submit=True):
+        apelido = st.text_input("Apelido (ID único):", key="apelido_c").strip().title()
+        posicao = st.selectbox("Posição:", ['Linha', 'Goleiro'], key="pos_c")
+        telefone = st.text_input("Telefone: (Opcional)", key="tel_c").strip()
+        nome_completo = st.text_input("Nome Completo: (Opcional)", key="nome_c").strip().title()
+        status = st.checkbox("É Mensalista?", value=True)
+        submitted = st.form_submit_button("Cadastrar jogador")
+
+        if submitted:
+            if not apelido or apelido in JOGADORES:
+                st.error(f"❌ Apelido {apelido} inválido ou já em uso.")
+                return
+            
+            # A única validação obrigatória é o Apelido
+            
+            JOGADORES[apelido] = {
+                'nome': nome_completo or None, # Salva como None se estiver vazio
+                'telefone': telefone or None, # Salva como None se estiver vazio
+                'posicao': posicao,
+                'mensalista': status,
+                'data_cadastro': datetime.now().strftime("%Y-%m-%d"),
+                'stats': {'gols': 0, 'amarelos': 0, 'vermelhos': 0}
+            }
+            
+            mes_atual = datetime.now().strftime("%Y-%m")
+            valor_devido = 0.0 if posicao == 'Goleiro' else VALOR_MENSALIDADE_ATUAL
+            
+            if apelido not in FINANCEIRO['mensalidades'] and JOGADORES[apelido]['mensalista'] == True:
+                FINANCEIRO['mensalidades'][apelido] = {}
+                
+                FINANCEIRO['mensalidades'][apelido][mes_atual] = {
+                    'pago': False,
+                    'valor_devido': valor_devido,
+                    'data_pagamento': None
+                }  
+                salvar_dados(FINANCEIRO, arquivo_financeiro)
+            
+            salvar_dados(JOGADORES, arquivo_jogadores)
+            
+            
+            st.success(f'✅ Jogador "{apelido}" na posição "{posicao}" adicionado com sucesso!')
+
+def alterar_dados_jogador_ui():
+    """Permite alterar o nome, telefone, posição ou status de mensalista de um jogador."""
+    st.subheader("✏️ Alterar Dados de Jogador", anchor='alterar-jogador')
+    JOGADORES = st.session_state['jogadores']
+    FINANCEIRO = st.session_state['financeiro']
+    VALOR_MENSALIDADE_ATUAL = FINANCEIRO['config']['mensalidade_atual']
+
+    apelido_escolhido = st.selectbox("Selecione o jogador para alterar:", 
+                                    [''] + sorted(JOGADORES.keys()))
+    
+    if apelido_escolhido:
+        jogador = JOGADORES[apelido_escolhido]
+        
+        with st.form("alterar_jogador_form", clear_on_submit=True):
+            st.markdown(f'**Dados atuais de "{apelido_escolhido.upper()}":**')
+            
+            novo_nome = st.text_input("Novo Nome Completo:", value=jogador['nome'])
+            novo_telefone = st.text_input("Novo Telefone:", value=jogador['telefone'])
+            nova_posicao = st.selectbox("Nova Posição:", 
+                                        ['Linha', 'Goleiro'], 
+                                        index=0 if jogador['posicao'] == 'Linha' else 1)
+            novo_status_mensalista = st.checkbox("É Mensalista?", value=jogador['mensalista'])
+
+            submitted = st.form_submit_button("Salvar Alterações")
+
+            if submitted:
+                # Lógica de atualização
+                jogador['nome'] = novo_nome
+                jogador['telefone'] = novo_telefone
+                
+                # Ajuste de posição e status
+                old_pos = jogador['posicao']
+                old_mensalista = jogador['mensalista']
+                
+                jogador['posicao'] = nova_posicao
+                jogador['mensalista'] = novo_status_mensalista
+                
+                # Lógica de ajuste financeiro (se status/posição mudou)
+                if old_pos != nova_posicao or old_mensalista != novo_status_mensalista:
+                    mes_atual = datetime.now().strftime("%Y-%m")
+                    valor_devido = 0.0
+                    
+                    if apelido_escolhido not in FINANCEIRO['mensalidades'] and nova_posicao == 'Linha':
+                        FINANCEIRO['mensalidades'][apelido_escolhido]= {}
+                        FINANCEIRO['mensalidades'][apelido_escolhido][mes_atual] = {
+                            'pago': False,
+                            'valor_devido': valor_devido,
+                            'data_pagamento': None
+                            }
+                    
+                    if apelido_escolhido in FINANCEIRO['mensalidades'] and mes_atual in FINANCEIRO['mensalidades'][apelido_escolhido] and not FINANCEIRO['mensalidades'][apelido_escolhido][mes_atual]['pago']:
+                        
+                        if novo_status_mensalista and nova_posicao == 'Linha':
+                            valor_devido = VALOR_MENSALIDADE_ATUAL
+                            
+                        FINANCEIRO['mensalidades'][apelido_escolhido][mes_atual]['valor_devido'] = valor_devido
+                        st.warning("⚠️ Status/Posição alterada. O valor da mensalidade do mês atual foi ajustado.")
+
+                salvar_dados(JOGADORES, arquivo_jogadores)
+                salvar_dados(FINANCEIRO, arquivo_financeiro)
+                st.success(f"✅ Dados de '{apelido_escolhido}' atualizados com sucesso.")
+
+def listas_e_qtd_mensalistas_convidados():
+    JOGADORES = st.session_state['jogadores']
+    
+    mensalistas = sorted([a for a, d in JOGADORES.items() if d.get('mensalista', False)])
+    convidados = sorted([a for a, d in JOGADORES.items() if not d.get('mensalista', False)])
+    qtd_mensalistas = len(mensalistas)
+    qtd_convidados = len(convidados)
+    return mensalistas, convidados, qtd_mensalistas, qtd_convidados
+
+def gerar_dados_para_lista_global_jogadores(tipo_lista):
+    JOGADORES = st.session_state['jogadores']
+    FINANCEIRO = st.session_state['financeiro']
+    SUMULAS = st.session_state['sumulas']
+    
+    mes_atual = datetime.now().strftime("%Y-%m")
+
+    if not JOGADORES:
+        st.info("Nenhum jogador cadastrado.")
+        return
+
+    dados_tabela = []
+    jogadores_ordenados = sorted(JOGADORES.items())
+    
+    for apelido, dados in jogadores_ordenados:
+        status = "N/A"
+        gols, amarelos, vermelhos = 0, 0, 0
+
+        if dados.get('mensalista', False):
+            info_mensalidade = FINANCEIRO['mensalidades'].get(apelido, {}).get(mes_atual, {})
+            
+            if not info_mensalidade:
+                mensalidade_atual = FINANCEIRO['config']['mensalidade_atual']
+                valor_devido = 0.0 if dados['posicao'] == 'Goleiro' else mensalidade_atual
+                FINANCEIRO['mensalidades'][apelido][mes_atual] = {
+                    "pago": False,
+                    "valor_devido": valor_devido,
+                    "data_pagamento": None
+                    }
+                salvar_dados(FINANCEIRO, arquivo_financeiro)
+            
+            if info_mensalidade:
+                status = f"PAGO ({mes_atual})" if info_mensalidade.get('pago') else f"DEVEDOR R$ {info_mensalidade.get('valor_devido', 0.0):.2f}"
+            else:
+                status = "N/A (Novo Mês)"
+        
+        if 'stats' in dados:
+            gols = dados['stats']['gols']
+            amarelos = dados['stats']['amarelos']
+            vermelhos = dados['stats']['vermelhos']
+
+        linha = {
+            'Apelido': apelido,
+            'Posicao': dados['posicao'],
+            'Status Jogador': 'Mensalista' if dados.get('mensalista', False) else 'Convidado',
+            'Gols': gols,
+            '🟨': amarelos,
+            '🟥': vermelhos,
+        }
+        
+        if tipo_lista == 'cadastro':
+            linha[f'Status Mensalidade ({mes_atual})'] = status
+        
+        dados_tabela.append(linha)
+    
+    df = pd.DataFrame(dados_tabela)
+    
+    return df
+
+def listar_jogadores_ui(tipo_lista):
+    df = gerar_dados_para_lista_global_jogadores(tipo_lista)
+    with st.form('lista_jogadores'):
+        submitted = st.form_submit_button(f'Atualizar dados')
+        st.dataframe(df, use_container_width=True, hide_index=True)
+        
+        if submitted:
+            st.rerun()
+    return
---
+++ futebol_app.py
@@ -0,0 +1,238 @@
+import streamlit as st
+import bcrypt as bc
+from datetime import datetime
+from f_app_modules import (
+    variaveis,
+    adicionar_jogador_ui, alterar_dados_jogador_ui, listas_e_qtd_mensalistas_convidados, listar_jogadores_ui, salvar_dados, registrar_pagamento_mensalidade_ui, registrar_pagamento_convite_ui,
+    calcular_geral_ui, editar_estatisticas_sumula_ui, exibir_estatistica_anual,
+    criar_sumula_simples_ui, cadastrar_jogadores_na_sumula_ui,
+    gerar_relatorio_ui,
+    registrar_gasto_comum_ui, listar_gastos_comuns_ui,
+    alterar_config_financeira_ui,
+    carregar_dados,
+    sidebar_logo
+)
+
+#--- VARIÁVEIS DE CONFIGURAÇÃO (Constantes) ---
+ARQUIVO_DADOS_JOGADORES, ARQUIVO_DADOS_FINANCEIRO, ARQUIVO_DADOS_SUMULAS, ARQUIVO_DADOS_USUARIOS = variaveis()
+
+def inicializar_estado():
+    # Trecho de FUNÇÕES DE PERSISTÊNCIA e INICIALIZAÇÃO DE ESTADO
+    
+    # 1. Carrega Jogadores
+    if 'jogadores' not in st.session_state:
+        st.session_state['jogadores'] = carregar_dados(ARQUIVO_DADOS_JOGADORES)
+        
+    # 2. Carrega Financeiro
+    if 'financeiro' not in st.session_state:
+        FINANCEIRO = carregar_dados(ARQUIVO_DADOS_FINANCEIRO)
+        
+        if 'mensalidades' not in FINANCEIRO:
+            FINANCEIRO['mensalidades'] = {}
+        if 'gastos_comuns' not in FINANCEIRO:
+            FINANCEIRO['gastos_comuns'] = []
+        if 'convites' not in FINANCEIRO:
+            FINANCEIRO['convites'] = {}
+            
+        if 'config' not in FINANCEIRO:
+            VALOR_MENSALIDADE_ATUAL = 20.00
+            VALOR_CONVITE_ATUAL = 10.00
+            DATA_ALT_MENSALIDADE = datetime.now().strftime("%Y-%m-%d")
+            DATA_ALT_CONVITE = datetime.now().strftime("%Y-%m-%d")
+            
+            FINANCEIRO['config'] = {
+                'mensalidade_atual': VALOR_MENSALIDADE_ATUAL, 
+                'valor_convite_atual': VALOR_CONVITE_ATUAL, 
+                'data_alt_mensalidade': DATA_ALT_MENSALIDADE,
+                'data_alt_convite': DATA_ALT_CONVITE
+            }
+            
+        st.session_state['financeiro'] = FINANCEIRO
+        
+    # 3. 🆕 Carrega Súmulas
+    if 'sumulas' not in st.session_state:
+        st.session_state['sumulas'] = carregar_dados(ARQUIVO_DADOS_SUMULAS)
+    
+    # 4. Carrega Usuários
+    if 'usuarios' not in st.session_state:
+        st.session_state['usuarios'] = carregar_dados(ARQUIVO_DADOS_USUARIOS)
+
+def login_page():
+    if 'usuarios' not in st.session_state:
+        st.session_state['usuarios'] = carregar_dados(ARQUIVO_DADOS_USUARIOS)
+    
+    USUARIOS = st.session_state['usuarios']
+    
+    # Inicializa o estado de autenticação se ainda não existir
+    if 'authenticated' not in st.session_state:
+        st.session_state['authenticated'] = False
+        st.session_state['username'] = None
+        
+    if st.session_state['authenticated']:
+        # Se autenticado, chama a aplicação principal
+        main_app() 
+        return
+
+    # --- 3. TELA DE LOGIN (Executado apenas se 'authenticated' for False)
+    if not USUARIOS:
+        senha_admin = gerar_hash_senha('admin')
+        USUARIOS['admin'] = {
+            'senha': senha_admin
+        }
+        
+        salvar_dados(USUARIOS, ARQUIVO_DADOS_USUARIOS)
+        st.success('Parabéns! Esse é seu primeiro acesso ao sistema. Entre com o usuário: Admin e senha: admin')
+        
+    sidebar_logo()
+    st.markdown('---')
+    col_esq, col_meio, col_dir = st.columns([1, 2, 1])
+    with col_meio:
+        with st.form("login_form", width=500):
+            st.subheader(":closed_lock_with_key: Login de usuário")
+            username = st.text_input("Usuário: ").lower()
+            password = st.text_input("Senha:", type="password")
+            submitted = st.form_submit_button("Entrar")
+
+            if submitted:
+                if username in USUARIOS:
+                    senha_armazenada = USUARIOS[username]['senha']
+                    
+                    if verificar_senha(password, senha_armazenada):
+                                        
+                    # Credenciais válidas: Atualiza o estado
+                        st.session_state['authenticated'] = True
+                        st.session_state['username'] = username
+                        st.success(f"Bem-vindo(a), {username}!")
+                    
+                        # Força a reexecução para carregar a main_app
+                        st.rerun() 
+                    else:
+                        st.error("Usuário ou senha inválidos.")
+                else:
+                    st.error("Usuário ou senha inválidos.")
+
+def gerar_hash_senha(senha_pura):
+    # O bcrypt.gensalt() gera um "salt" (valor aleatório) que garante que duas senhas 
+    # idênticas tenham hashes diferentes, aumentando a segurança.
+    salt = bc.gensalt()
+    
+    # O hash é gerado com a senha pura e o salt
+    senha_hasheada = bc.hashpw(senha_pura.encode('utf-8'), salt)
+    
+    # Armazene o resultado em string para JSON ou dicionário
+    return senha_hasheada.decode('utf-8')
+
+def verificar_senha(senha_pura, hash_armazenada):
+    # A senha pura e a hash armazenada devem ser codificadas de volta para bytes
+    return bc.checkpw(senha_pura.encode('utf-8'), hash_armazenada.encode('utf-8'))
+
+def main_app():
+    ### Define a interface e o roteamento da aplicação Streamlit.
+    # Chama a função para garantir que os dados estão carregados no st.session_state
+    inicializar_estado() 
+    
+    st.set_page_config(layout="wide", page_title="Amigos do CNPA", page_icon='⚽')
+    
+    # --- MENU LATERAL (Sidebar) ---
+    usuario = str(st.session_state['username']).title()
+    
+    sidebar_logo()
+    with st.sidebar:
+        #st.markdown('---')
+        
+        col_usu, col_bv = st.columns([1,6])
+        with col_usu:
+            st.image('imgs/usuario.png')
+        with col_bv:
+            st.markdown(f'***{usuario}***')   
+            
+    
+    menu_selecionado = st.sidebar.radio(
+        "",
+        [
+            "🏠 Dashboard",
+            "🤝 Cadastro de Jogadores",
+            "💲 Mensalidades e Convites",
+            "⚽ Súmulas e Estatísticas",
+            "📈 Relatórios Financeiros",
+            "💸 Controle de gasto comum",
+            "⚙️ Configurações"
+        ]
+    )
+    #st.sidebar.markdown('---')
+    st.sidebar.button("Sair do sistema", on_click=logout)
+    
+    # Roteamento
+    if menu_selecionado == "🏠 Dashboard":
+        st.header("🏠 Dashboard")
+        st.markdown("---")
+        calcular_geral_ui()
+        
+        
+    elif menu_selecionado == "🤝 Cadastro de Jogadores":
+        st.header('📋 CADASTRO DE JOGADORES', anchor='lista_jogadores')
+        col1, col2 = st.columns([1, 3]) # links de atalhos para CADASTRAR e ALTERAR dados dos jogadores
+        with col1:
+            st.markdown("[Cadastrar Jogador](#cadastrar-jogador)")
+        with col2:
+            st.markdown("[Alterar dados do Jogador](#alterar-jogador)")
+        
+        st.subheader("Jogadores cadastrados, estatísticas globais e status financeiro")
+        a, b, qtd_mensalistas, qtd_convidados = listas_e_qtd_mensalistas_convidados()
+        st.markdown(f'Mensalista(s): {qtd_mensalistas} | Convidado(s): {qtd_convidados}')
+        
+        listar_jogadores_ui('cadastro')
+        col_cad, col_list = st.columns(2)
+        with col_cad:
+            adicionar_jogador_ui()
+        with col_list:
+            alterar_dados_jogador_ui()
+        
+    elif menu_selecionado == "💲 Mensalidades e Convites":
+        col_mens, col_conv = st.columns(2)
+        with col_mens:
+            registrar_pagamento_mensalidade_ui()
+        with col_conv:
+            registrar_pagamento_convite_ui()
+        
+    elif menu_selecionado == "⚽ Súmulas e Estatísticas":
+        st.header("⚽ Súmulas e Estatísticas")
+        st.markdown("---")
+        col1, col2 = st.columns([1, 3])
+        with col1:
+            criar_sumula_simples_ui()
+        
+        with col2:
+            cadastrar_jogadores_na_sumula_ui()
+        st.markdown("---")
+        st.subheader("📊 ETATÍSTICAS")
+        col_d, col_g = st.columns(2)
+        with col_d:
+            st.subheader(':calendar: Por partida')
+            editar_estatisticas_sumula_ui()
+        with col_g:
+            st.subheader(':calendar: Anual')
+            exibir_estatistica_anual()
+            
+        st.subheader(':globe_with_meridians: Globais')
+        listar_jogadores_ui('estatisticas')
+
+    elif menu_selecionado == "📈 Relatórios Financeiros":
+        gerar_relatorio_ui()
+    
+    elif menu_selecionado == "💸 Controle de gasto comum":
+        col1, col2 = st.columns(2)
+        with col1:
+            registrar_gasto_comum_ui()
+        with col2:
+
+            listar_gastos_comuns_ui()
+    elif menu_selecionado == "⚙️ Configurações":
+        alterar_config_financeira_ui()
+
+def logout():
+    st.session_state['authenticated'] = False
+    st.session_state['username'] = None
+    
+if __name__ == "__main__":
+    login_page()
